2031ca90e7d59614af8a06513dd66c9ff7b34354
Anfacibry <jogos.fabriciocintra@gmail.com>
2022-03-03 14:44:02 -0300

    Atualizando tela principal

--- lib/pages/tela_principal.dart
+++ lib/pages/tela_principal.dart
@@ -1,25 +1,46 @@
 import 'package:flutter/material.dart';
 
-class TelaPrincipal extends StatelessWidget {
+class TelaPrincipal extends StatefulWidget {
   const TelaPrincipal({Key? key}) : super(key: key);
 
   @override
+  State<TelaPrincipal> createState() => _TelaPrincipalState();
+}
+
+class _TelaPrincipalState extends State<TelaPrincipal> {
+  final TextEditingController textoRecebido = TextEditingController();
+
+  void observar() {
+    setState(() {
+      textoRecebido;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Center(
         child: Padding(
-          padding: EdgeInsets.symmetric(horizontal: 20),
-          child: TextField(
-            decoration: InputDecoration(
-              labelText: "+ Item",
-              hintText: "Digite um item",
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.all(
-                  Radius.circular(10),
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(textoRecebido.text),
+              TextField(
+                controller: textoRecebido,
+                decoration: const InputDecoration(
+                  labelText: "+ Item",
+                  hintText: "Digite um item",
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(10),
+                    ),
+                  ),
                 ),
+                keyboardType: TextInputType.emailAddress,
               ),
-            ),
-            keyboardType: TextInputType.number,
+              ElevatedButton(onPressed: observar, child: const Text("Salvar"))
+            ],
           ),
         ),
       ),
