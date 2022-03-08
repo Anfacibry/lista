3e8569a87045ec25dcb5c4483f2f27d131c9b019
Anfacibry <jogos.fabriciocintra@gmail.com>
2022-03-08 16:32:24 -0300

    atualizando tela para receber o item e a horas

--- lib/pages/tela_principal.dart
+++ lib/pages/tela_principal.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:lista/models/conteudo.dart';
+
+import '../widgets/lista_itens.dart';
 
 class TelaPrincipal extends StatefulWidget {
   const TelaPrincipal({Key? key}) : super(key: key);
@@ -10,85 +13,87 @@ class TelaPrincipal extends StatefulWidget {
 class _TelaPrincipalState extends State<TelaPrincipal> {
   TextEditingController textoRecebido = TextEditingController();
 
-  List<String> lista = [];
+  List<ItenAdd> lista = [];
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: Center(
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 10),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Row(
-              children: [
-                Expanded(
-                  flex: 4,
-                  child: TextField(
-                    decoration: const InputDecoration(
-                      labelText: "Adicione uma tarefa",
-                      border: OutlineInputBorder(),
-                    ),
-                    keyboardType: TextInputType.name,
-                    controller: textoRecebido,
-                  ),
-                ),
-                const SizedBox(width: 10),
-                Expanded(
-                  child: ElevatedButton(
-                    onPressed: () {
-                      setState(() {
-                        lista.add(textoRecebido.text);
-                      });
-                    },
-                    child: const Icon(Icons.add),
-                    style: ElevatedButton.styleFrom(
-                      primary: const Color(0xFF00D7F3),
-                      fixedSize: const Size(60, 60),
-                    ),
-                  ),
-                )
-              ],
-            ),
-            Flexible(
-              child: ListView(
-                shrinkWrap: true,
+    return SafeArea(
+      child: Scaffold(
+          body: Center(
+        child: Padding(
+          padding:
+              const EdgeInsets.only(left: 10, top: 15, right: 10, bottom: 10),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Row(
                 children: [
-                  for (String nomesListas in lista)
-                    ListTile(
-                      title: Text(
-                        nomesListas,
-                        style: const TextStyle(
-                          color: Colors.black,
-                        ),
+                  Expanded(
+                    flex: 4,
+                    child: TextField(
+                      decoration: const InputDecoration(
+                        labelText: "Adicione uma tarefa",
+                        border: OutlineInputBorder(),
+                      ),
+                      keyboardType: TextInputType.name,
+                      controller: textoRecebido,
+                    ),
+                  ),
+                  const SizedBox(width: 10),
+                  Expanded(
+                    child: ElevatedButton(
+                      onPressed: () {
+                        setState(() {
+                          ItenAdd item = ItenAdd(
+                              titulo: textoRecebido.text,
+                              dataHora: DateTime.now());
+                          lista.add(item);
+                        });
+                      },
+                      child: const Icon(Icons.add),
+                      style: ElevatedButton.styleFrom(
+                        primary: const Color(0xFF00D7F3),
+                        fixedSize: const Size(60, 60),
                       ),
                     ),
+                  )
                 ],
               ),
-            ),
-            Row(
-              children: [
-                const Expanded(
-                  flex: 3,
-                  child: Text("Você possui 0 tarefas pendentes"),
+              Flexible(
+                child: ListView(
+                  shrinkWrap: true,
+                  children: [
+                    for (ItenAdd nomesListas in lista)
+                      ListaDeItens(
+                        item: nomesListas,
+                      ),
+                  ],
                 ),
-                const SizedBox(width: 10),
-                Expanded(
-                  child: ElevatedButton(
-                    onPressed: () {},
-                    child: const Text("Limpar tarefas"),
-                    style: ElevatedButton.styleFrom(
-                      primary: const Color(0xFF00D7F3),
-                      fixedSize: const Size(60, 60),
-                    ),
+              ),
+              Row(
+                children: [
+                  Expanded(
+                    flex: 3,
+                    child:
+                        Text("Você possui ${lista.length} tarefas pendentes"),
                   ),
-                )
-              ],
-            ),
-          ],
+                  const SizedBox(width: 10),
+                  Expanded(
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      child: const Text("Limpar tarefas"),
+                      style: ElevatedButton.styleFrom(
+                        primary: const Color(0xFF00D7F3),
+                        fixedSize: const Size(60, 60),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ],
+          ),
         ),
-      ),
-    ));
+      )),
+    );
   }
 }
