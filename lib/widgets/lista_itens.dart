97449422f53a016b43c341be02a40861d4070ed9
Anfacibry <jogos.fabriciocintra@gmail.com>
2022-03-19 14:53:05 -0300

    atualizando lista

--- lib/widgets/lista_itens.dart
+++ lib/widgets/lista_itens.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:intl/intl.dart';
 import 'package:lista/models/conteudo.dart';
 
@@ -12,32 +13,41 @@ class ListaDeItens extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        color: const Color.fromARGB(255, 19, 79, 87),
-        borderRadius: BorderRadius.circular(10),
-      ),
-      padding: const EdgeInsets.all(10),
-      margin: const EdgeInsets.all(4),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            DateFormat("dd/MM/yy - HH:mm").format(item.dataHora),
-            style: const TextStyle(
-              fontSize: 15,
-              color: Colors.white,
+    return Slidable(
+      actionPane: const SlidableStrechActionPane(),
+      secondaryActions: const [
+        IconSlideAction(
+          color: Color.fromARGB(255, 235, 104, 95),
+          icon: Icons.delete,
+          onTap: null,
+        )
+      ],
+      child: Container(
+        decoration: BoxDecoration(
+          color: const Color.fromARGB(255, 19, 79, 87),
+          borderRadius: BorderRadius.circular(10),
+        ),
+        padding: const EdgeInsets.all(10),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              DateFormat("dd/MM/yy - HH:mm").format(item.dataHora),
+              style: const TextStyle(
+                fontSize: 15,
+                color: Colors.white,
+              ),
             ),
-          ),
-          Text(
-            item.titulo,
-            style: const TextStyle(
-              fontSize: 24,
-              fontWeight: FontWeight.bold,
-              color: Colors.white,
+            Text(
+              item.titulo,
+              style: const TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
